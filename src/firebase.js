5cb4748e7535dfd2df4113bd927b18f491f79129
Abhishek Kumar <abhikr.ak.2001@gmail.com>
2024-07-10 00:00:57 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4buMyiJi6ZvbpFoNZax3TklOJqepQ6t0",
+  authDomain: "make-my-trip-e9149.firebaseapp.com",
+  projectId: "make-my-trip-e9149",
+  storageBucket: "make-my-trip-e9149.appspot.com",
+  messagingSenderId: "837314370965",
+  appId: "1:837314370965:web:62a4257f316fb6cb066a9c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
